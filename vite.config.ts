563beef8b83ffe66765ee51afa85eb80fcfed647
Vamsi Krishna Kodimela <vamsikrishnakdml@gmail.com>
2025-08-07 12:38:04 +0530

    Add preview configuration to Vite for allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['optiq-assignment-321677150743.asia-northeast1.run.app']
+  }
 })
